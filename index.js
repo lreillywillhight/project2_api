780d7da935ef7345dff03216324ac65b44e910a0
lreillywillhight <62311980+lreillywillhight@users.noreply.github.com>
2020-07-01 15:45:59 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,85 @@
+// NPM libraries
+require('dotenv').config()
+
+const Express = require('express');
+const ejsLayouts = require("express-ejs-layouts")
+const helmet = require('helmet')
+const session = require('express-session')
+const flash = require('connect-flash')
+const passport = require('./config/ppConfig')
+const db = require('./models')
+const isLoggedIn = require('./middleware/isLoggedIn')
+const SequelizeStore = require('connect-session-sequelize')(session.Store)
+// const flash = require("connect-flash")
+
+// app setup
+const app = Express()
+app.use(Express.urlencoded({ extended: false }))
+app.use(Express.static(__dirname + "/public"))
+app.set('view engine', 'ejs')
+app.use(ejsLayouts)
+app.use(require('morgan')('dev'))
+app.use(helmet())
+// app.use(flash())
+
+// create new instance of class Sequelize Store
+const sessionStore = new SequelizeStore({
+    db: db.sequelize,
+    expiration: 1000 * 60 * 30
+})
+
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    store: sessionStore,
+    resave: false,
+    saveUninitialized: true
+}))
+
+sessionStore.sync()
+
+
+// initialize passport and session info
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(flash())
+
+app.use(function (req, res, next) {
+    res.locals.alert = req.flash()
+    res.locals.currentUser = req.user
+
+    next();
+})
+
+
+
+
+
+// ROUTES
+
+app.get('/', (req, res) => {
+    // check to see if user is logged in
+    res.render('index', { messages: req.flash('info')})
+})
+
+app.get('/profile', isLoggedIn, function (req, res) {
+    res.render('profile')
+})
+
+app.get('/flash', function (req, res) {
+    req.flash('index', 'Flash is back!')
+    res.redirect('/');
+})
+
+// app.get('/', function (req, res) {
+//     res.render('index', { messages: req.flash('info') });
+// })
+
+
+// INCLUDE AUTH CONTROLLERS
+
+app.use('/auth', require('./controllers/auth'))
+
+
+// INITIALIZE APP ON PORT   
+
+app.listen(process.env.PORT || 3000, function (port) { console.log(`port ${process.env.PORT}`) })
---
+++ middleware/isLoggedIn.js
@@ -0,0 +1,13 @@
+// write a function we are going to use as middleware
+module.exports = function(req,res,next) {
+    if (!req.user) {
+        req.flash('error', 'you must be logged in to view this page')
+        res.redirect('/auth/login')
+    } else {
+        next()
+    }
+}
+// check and see if we have a user var set
+// if we do we will allow our app to carry on
+// but if we don't we will let user know they have to be logged in to access
+// redirect user to /auth/login
